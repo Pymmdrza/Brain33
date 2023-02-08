2262f75cacbe8e762be44f1e7e45c20917c94f38
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-02-08 12:43:19 +0300

    Installing Package On `Debian/Linux`

---
+++ INSTALL.sh
@@ -0,0 +1,4 @@
+sudo apt-get update&&apt-get upgrade -y
+sudo pip3 install requests-html bit ecdsa rich
+sudo pip3 install requests-random-user-agent
+echo "Installing Package Successfully [MMDRZA.COM]"
